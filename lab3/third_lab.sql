c6c8439b4d4f8cbeb3f7b7da8da6d139c47af2e0
Mira <141563756+oduvanchikm@users.noreply.github.com>
2024-10-25 22:43:11 +0300

    Update third_lab.sql

--- lab3/third_lab.sql
+++ lab3/third_lab.sql
@@ -1,1 +1,46 @@
-d
+create table Artierts (
+	ArtiertID int primary key,
+	ArtietName text not null,
+	DatesOfLife date
+);
+
+create table Galleries (
+	GalleryID int primary key,
+	Address text not null,
+	ContactInfo text
+);
+
+create table Visitors (
+	VisitorID int primary key,
+	VisitorName text not null,
+	VisitorAge int not null,
+	Cintactinfo text not null
+)
+
+create table ArtObjects (
+	ArtObjectID int primary key,
+	ArtiertID int,
+	GalleryID int,
+	Name text not null,
+	Description text not null,
+	foreign key (ArtiertID) references Artierts(ArtiertID),
+	foreign key (GalleryID) references Galleries(GalleryID)
+);
+
+create table Tours (
+	TourID int primary key,
+	TourName text not null,
+	TourDate date not null,
+	Quantity int,
+	GalleryID int,
+	foreign key (GalleryID) references Galleries(GalleryID)
+);
+
+create table Visits (
+	VisitID int primary key,
+	GalleryID int,
+	VisitorID int,
+	VisitDate date not null,
+	foreign key (GalleryID) references Galleries(GalleryID),
+	foreign key (VisitorID) references Visitors(VisitorID)
+);
